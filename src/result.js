329d6a18ea08626be27f96a6359879f217513675
kkanazaw <new.frontier2006@gmail.com>
2019-02-21 15:08:33 +0900

    refs #11 テスト用コードを削除

--- src/result.js
+++ src/result.js
@@ -1,9 +1,6 @@
 import $ from 'jquery';
 
 $(document).ready(() => {
-  //buildテスト用
-  const debug = "debug";
-  //ここまで
   const calcScore = () => {
     const scores = Array(11).fill(0);
     scores[10] = 5;
