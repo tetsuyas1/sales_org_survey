79f83c3fcb5d7c67745248b6c236349343ce3134
kkanazaw <new.frontier2006@gmail.com>
2019-02-26 16:49:07 +0900

    [WIP] refs #13 jqueryを二重に読み込んでいたところをwebpackに寄せた(cssはcdnから読み込む)
    Chart.jsは元のhtmlファイルと共有しているようだったのでそのままにした

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,12 @@
 const path = require('path');
-
+const webpack = require("webpack");
 const js = {
   mode: 'development',
   entry: {
     index: path.resolve(__dirname, 'src', 'index.js'),
     q: path.resolve(__dirname, 'src', 'q.js'),
     result: path.resolve(__dirname, 'src', 'result.js')
+    
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
@@ -22,7 +23,38 @@ const js = {
         ]
       }
     ]
-  }
+  },
 };
 
-module.exports = js;
+const vendorScript = {
+  mode: 'development',
+  entry: ["jquery", "popper.js", "bootstrap", "tippy.js"],
+  output: {
+    filename: "vendor.js",
+    path: path.join(__dirname, "dist")
+  },
+  module: {
+    rules: [{
+      test: require.resolve("jquery"),
+      use: [{
+        loader: "expose-loader", // global に jQuery / $ を公開
+        options: "jQuery"
+      }, {
+        loader: "expose-loader",
+        options: "$"
+      }]
+    }]
+  },
+  plugins: [
+    // bootstrap のコードから jQuery が直接見えるように
+    // http://getbootstrap.com/docs/4.0/getting-started/webpack/#importing-javascript
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery",
+      "window.jQuery": "jquery",
+      Popper: ["popper.js", "default"]
+    })
+  ]
+};
+
+module.exports = [js, vendorScript]
